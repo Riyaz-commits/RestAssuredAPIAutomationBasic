54b90c4b20e8e60269b571894c8449d9dc7cdf08
Riyaz Basha Shaik <skriyazbasha91@gmail.com>
2021-08-08 10:29:12 +0530

    1st APi commit

---
+++ src/main/java/APIGroup/ProjectAPI/App.java
@@ -0,0 +1,13 @@
+package APIGroup.ProjectAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
